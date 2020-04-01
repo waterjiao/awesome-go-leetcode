b7776b5e90df020e6720b26975f3eb3b5fb21463
menger <785672729@qq.com>
2020-04-01 23:03:36 +0800

    0003 finish

---
+++ Algorithms/0003. Longest Substring Without Repeating Characters/longest_substring_without_repeating_characters.go
@@ -0,0 +1,31 @@
+package problem003
+
+func lengthOfLongestSubstring(s string) int {
+	var tempList = make([]rune,0)
+	var maxLen = 0
+	for _, ch := range s {
+		index, v := stringInList(tempList, ch)
+		if v {
+			if len(tempList) > maxLen {
+				maxLen = len(tempList)
+			}
+			tempList = tempList[index+1:]
+			tempList = append(tempList, ch)
+		} else {
+			tempList = append(tempList, ch)
+			if len(tempList) > maxLen {
+				maxLen = len(tempList)
+			}
+		}
+	}
+	return maxLen
+}
+
+func stringInList(tempList []rune, value rune) (int, bool) {
+	for index, v := range tempList {
+		if v == value {
+			return index, true
+		}
+	}
+	return -1, false
+}
