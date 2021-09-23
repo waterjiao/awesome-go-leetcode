f7292d2c3377559be24746d4d8145c859ac0dd28
menger <785672729@qq.com>
2021-09-23 23:01:29 +0800

    20210923

--- Algorithms/0509.Fibonacci-Number/fibonacci-number.go
+++ Algorithms/0509.Fibonacci-Number/fibonacci-number.go
@@ -18,3 +18,25 @@ func fibNum(n int, mano []int) int {
 	mano[n] = fibNum(n-1, mano) + fibNum(n-2, mano)
 	return mano[n]
 }
+
+func fib2(n int) int {
+	dp := make([]int, n+1)
+	dp[0], dp[1] = 0, 1
+	for i := 2; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
+
+func fib3(n int) int {
+	if n == 0 || n == 1 {
+		return n
+	}
+	pre, curr := 0, 1
+	for i := 2; i <= n; i++ {
+		sum := pre + curr
+		pre = curr
+		curr = sum
+	}
+	return curr
+}
